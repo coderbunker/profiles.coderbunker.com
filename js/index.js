1d4d51ad6dca5b756bf6d2cb96b99fd459517a0e
Charles Carrier <charles.e.carrier@gmail.com>
2018-10-17 13:58:09 +0800

    search displays just username and profile link

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 const inputArea = document.getElementById("search");
 const searchBtn = document.querySelector(".search-btn");
 const finalTextArea = document.querySelector(".final-text");
+const allUsersArray = [];
 
 var index = elasticlunr(function () {
     this.addField('fullname');
     this.addField('email');
     this.addField('keywords');
+    this.addField('slideUrl');
+    this.addField('profileUrl');
     this.setRef('id');
 });
 
@@ -13,7 +16,20 @@ var index = elasticlunr(function () {
 var doc1 = {
     "id": 1,
     "fullname": "Ricky Ng-Adam",
+    "altnames": [
+        "伍思力",
+        " Ricky",
+        " rngadam"
+    ],
+    "altemails": "rngadam@gmail.com",
     "email": "rngadam@coderbunker.com",
+    "github": "rngadam",
+    "linkedin": "https://www.linkedin.com/in/rngadam/",
+    "wechat": "rngadam",
+    "status": "Founder, Partner, Associate, Member, Villager, Speaker",
+    "rate": "RMB 700 /hour",
+    "availability": "20 to 40 hours a week",
+    "portfolio": "http://www.coderbunker.com",
     "keywords": [
         "China",
         "@coderbunker.com",
@@ -41,12 +57,21 @@ var doc1 = {
         "Ansible",
         "MVC"
     ],
+    "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g1992957c98_7_61",
+    "objectId": "g1992957c98_7_61",
+    "profileUrl": "https://drive.google.com/uc?id=1wN9kEJULLNqrofTcmtJ2BsIrzI3AWjGj"
 }
 
 var doc2 = {
     "id": 2,
     "fullname": "Peter Bray",
     "email": "peter@foxandgeese.com",
+    "github": "https://github.com/bluepeter",
+    "linkedin": "https://www.linkedin.com/in/bluepeter/",
+    "wechat": "petebray",
+    "status": "Freelancer",
+    "notes": "Remote and on-prem consulting, short-term & long-term",
+    "rate": "RMB700/hour + expenses",
     "keywords": [
         "Product Management",
         "Software Architecture",
@@ -79,6 +104,9 @@ var doc2 = {
         "CD",
         "CI"
     ],
+    "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g3e1fd9e088_0_0",
+    "objectId": "g3e1fd9e088_0_0",
+    "profileUrl": "https://drive.google.com/uc?id=1z8dlUGiFBzhLIx__e2_kprshOXtz-BWh"
 }
 
 index.addDoc(doc1);
@@ -87,8 +115,14 @@ index.addDoc(doc2);
 searchBtn.addEventListener("click", searchIt);
 
 function searchIt() {
+    let individualUserArray =[];
     const searchStr = inputArea.value;
     const matchArr = index.search(searchStr);  // here's the result
     console.log(matchArr);
-    finalTextArea.innerText = JSON.stringify(matchArr);  // too lazy to style the result ;P
+    for ( let i = 0; i < matchArr.length; i++ ) {
+        individualUserArray.push( { Name: matchArr[i].doc.fullname } )
+        individualUserArray.push( { Profile: matchArr[i].doc.profileUrl } )
+    };
+    console.log(allUsersArray);
+    finalTextArea.innerText = JSON.stringify(individualUserArray);  // too lazy to style the result ;P
 }
