f1818f41fc2cf31c8a9cb935d2b9b792abd1f3ab
Charles Carrier <charles.e.carrier@gmail.com>
2018-10-18 15:27:29 +0800

    updating using objectId as set ref and modifying display

--- js/index.js
+++ js/index.js
@@ -8,108 +8,144 @@ var index = elasticlunr(function () {
     this.addField('keywords');
     this.addField('slideUrl');
     this.addField('profileUrl');
-    this.setRef('id');
+    this.setRef('objectId');
 });
 
-// you can grab the data from frontend, but I'm too lazy to do that
-var doc1 = {
-    "id": 1,
-    "fullname": "Ricky Ng-Adam",
-    "altnames": [
-        "伍思力",
-        " Ricky",
-        " rngadam"
-    ],
-    "altemails": "rngadam@gmail.com",
-    "email": "rngadam@coderbunker.com",
-    "github": "rngadam",
-    "linkedin": "https://www.linkedin.com/in/rngadam/",
-    "wechat": "rngadam",
-    "status": "Founder, Partner, Associate, Member, Villager, Speaker",
-    "rate": "RMB 700 /hour",
-    "availability": "20 to 40 hours a week",
-    "portfolio": "http://www.coderbunker.com",
-    "keywords": [
-        "China",
-        "@coderbunker.com",
-        "Coderbunker rngadam",
-        "Specialties",
-        "Javascript",
-        "software development",
-        "experience",
-        "testing Architecture",
-        "PostgreSQL",
-        "design",
-        "R&D",
-        "engineering team recruitment",
-        "building",
-        "shell",
-        "React",
-        "Python",
-        "C/C++",
-        "deployment",
-        "REST",
-        "Websockets",
-        "GraphQL",
-        "API servers",
-        "Linux",
-        "Ansible",
-        "MVC"
-    ],
-    "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g1992957c98_7_61",
-    "objectId": "g1992957c98_7_61",
-    "profileUrl": "https://drive.google.com/uc?id=1wN9kEJULLNqrofTcmtJ2BsIrzI3AWjGj"
+let membersArray = [
+	{
+      "fullname": "Ricky Ng-Adam",
+      "altnames": [
+          "伍思力",
+          " Ricky",
+          " rngadam"
+      ],
+      "altemails": "rngadam@gmail.com",
+      "email": "rngadam@coderbunker.com",
+      "github": "rngadam",
+      "linkedin": "https://www.linkedin.com/in/rngadam/",
+      "wechat": "rngadam",
+      "status": "Founder, Partner, Associate, Member, Villager, Speaker",
+      "rate": "RMB 700 /hour",
+      "availability": "20 to 40 hours a week",
+      "portfolio": "http://www.coderbunker.com",
+      "keywords": [
+          "@coderbunker.com",
+          "Coderbunker rngadam",
+          "Specialties",
+          "Javascript",
+          "software development",
+          "experience",
+          "testing Architecture",
+          "PostgreSQL",
+          "design",
+          "R&D",
+          "engineering team recruitment",
+          "building",
+          "shell",
+          "React",
+          "Python",
+          "C/C++",
+          "deployment",
+          "REST",
+          "Websockets",
+          "GraphQL",
+          "API servers",
+          "Linux",
+          "Ansible",
+          "MVC"
+      ],
+      "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g1992957c98_7_61",
+      "objectId": "g1992957c98_7_61",
+      "profileUrl": "https://drive.google.com/uc?id=1wN9kEJULLNqrofTcmtJ2BsIrzI3AWjGj"
+  },
+  {
+      "fullname": "Peter Bray",
+      "email": "peter@foxandgeese.com",
+      "github": "https://github.com/bluepeter",
+      "linkedin": "https://www.linkedin.com/in/bluepeter/",
+      "wechat": "petebray",
+      "status": "Freelancer",
+      "notes": "Remote and on-prem consulting, short-term & long-term",
+      "rate": "RMB700/hour + expenses",
+      "keywords": [
+          "Product Management",
+          "Software Architecture",
+          "peter@foxandgeese.com",
+          "Products",
+          "China",
+          "gifting",
+          "loyalty",
+          "project management",
+          "product",
+          "stars",
+          "plan",
+          "apps",
+          "payment",
+          "marketing automation",
+          "Starbucks",
+          "alignment",
+          "WeChat",
+          "microservices",
+          "team management",
+          "Grew SBUX iOS",
+          "Android",
+          "planning",
+          "Specialties",
+          "non-SQL",
+          "initiatives",
+          "cloud strategy",
+          "AWS",
+          "serverless",
+          "CD",
+          "CI"
+      ],
+      "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g3e1fd9e088_0_0",
+      "objectId": "g3e1fd9e088_0_0",
+      "profileUrl": "https://drive.google.com/uc?id=1z8dlUGiFBzhLIx__e2_kprshOXtz-BWh"
+  },
+  {
+    //   "id": 3,
+      "fullname": "Frederic Bazin",
+      "altnames": [
+          "Fred"
+      ],
+      "email": "frederic.bazin@coderbunker.com",
+      "linkedin": "https://cn.linkedin.com/in/frederic-bazin",
+      "rate": "RMB700/hour",
+      "status": "Partner, Associate, Member, Villager, Speaker, Freelancer",
+      "github": "lenzai",
+      "keywords": [
+          "Data Munging Architecture",
+          "@coderbunker.com",
+          "Specialties",
+          "bots",
+          "design",
+          "Refactoring",
+          "R&D",
+          "product management",
+          "experience",
+          "software development",
+          "SCRUM",
+          "code reviews"
+      ],
+      "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g39128f82d0_0_0",
+      "objectId": "g39128f82d0_0_0",
+      "profileUrl": "https://drive.google.com/uc?id=1rmpjrGCo1yW65CpdKBmdx01LIiEqi7EQ"
+  }
+]
+//
+
+
+
+
+
+
+for(let i = 0; i < membersArray.length; i++) {
+    console.log(JSON.stringify(membersArray[i]));
+    index.addDoc(membersArray[i]);
 }
 
-var doc2 = {
-    "id": 2,
-    "fullname": "Peter Bray",
-    "email": "peter@foxandgeese.com",
-    "github": "https://github.com/bluepeter",
-    "linkedin": "https://www.linkedin.com/in/bluepeter/",
-    "wechat": "petebray",
-    "status": "Freelancer",
-    "notes": "Remote and on-prem consulting, short-term & long-term",
-    "rate": "RMB700/hour + expenses",
-    "keywords": [
-        "Product Management",
-        "Software Architecture",
-        "peter@foxandgeese.com",
-        "Products",
-        "China",
-        "gifting",
-        "loyalty",
-        "project management",
-        "product",
-        "stars",
-        "plan",
-        "apps",
-        "payment",
-        "marketing automation",
-        "Starbucks",
-        "alignment",
-        "WeChat",
-        "microservices",
-        "team management",
-        "Grew SBUX iOS",
-        "Android",
-        "planning",
-        "Specialties",
-        "non-SQL",
-        "initiatives",
-        "cloud strategy",
-        "AWS",
-        "serverless",
-        "CD",
-        "CI"
-    ],
-    "slideUrl": "https://docs.google.com/presentation/d/1LvvSUc4VEU9hSSgi1x8PJWA_lBZ5ne7Few76U6CCFXI/edit#slide=id.g3e1fd9e088_0_0",
-    "objectId": "g3e1fd9e088_0_0",
-    "profileUrl": "https://drive.google.com/uc?id=1z8dlUGiFBzhLIx__e2_kprshOXtz-BWh"
-}
-
-index.addDoc(doc1);
-index.addDoc(doc2);
+console.log(index)
 
 searchBtn.addEventListener("click", searchIt);
 
